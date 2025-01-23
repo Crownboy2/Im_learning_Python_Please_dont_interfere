e978fd0e34999fdaf3272dd9db801b8942e8ec5a
Crown_boy2 <193437986+Crownboy2@users.noreply.github.com>
2025-01-23 21:11:08 +0300

    Delete test1 directory

--- test1/Give me your credit card password))).py
+++
@@ -1,7 +0,0 @@
-Your_name = input("your name: ")
-Your_card_PIN_code = input("Your card PIN code: ")
-
-print("Name: ")
-print(Your_name)
-print("Code: ")
-print(Your_card_PIN_code)
--- test1/My python learning test is in 10 minutes (Yes it's true) (3).py
+++
@@ -1,5 +0,0 @@
-name = "Oleg"
-age = 28
-
-print(name)
-print(age)
