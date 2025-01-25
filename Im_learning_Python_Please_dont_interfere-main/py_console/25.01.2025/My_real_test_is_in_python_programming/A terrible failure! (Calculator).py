52662ddb4bb7713e58e94b21bbb1d859683e42b8
Crown_boy2 <193437986+Crownboy2@users.noreply.github.com>
2025-01-25 13:21:01 +0300

    Add files via upload

---
+++ Im_learning_Python_Please_dont_interfere-main/py_console/25.01.2025/My_real_test_is_in_python_programming/A terrible failure! (Calculator).py
@@ -0,0 +1,41 @@
+a = not(False)         #(Here I used video lessons in order to at least remember something...)
+print(a)
+
+#/     a = not(0)      /
+#/     print(a)        /
+
+#(Here I am remembering for several hours how "Logic in Python" works...)
+
+b = True
+b2 = False
+
+#print(b or b2)
+
+b3 = False
+b4 = False
+
+#print(b3 or b4)
+
+b5 = True
+b6 = True
+
+print(b5 or b6)
+
+с = True
+с2 = True
+
+с3 = False
+с4 = False
+
+с5 = True
+с6 = False
+
+print(c and c2)
+print(c3 and c4)
+print(c5 and c6)
+
+#I'll be honest, I'm extremely dissatisfied with 
+#my knowledge throughout my studies 
+#(+ not enough time and not understanding English)
+#So I leave it as it is...
+#I need to remember absolutely everything for this month...
